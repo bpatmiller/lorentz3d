fb66cbd891ce187fcff0a102a053a9bdc492213f
Unknown <brendanmillerca@gmail.com>
2017-05-21 19:40:59 -0500

    added other attractors and a 'respawn' mode

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 // gui data
 var data = function() {
-  this.c = 28;
-  this.t = -6;
-  this.b = 2.67;
+  this.a = 28;
+  this.b = -6;
+  this.c = 2.67;
   this.points = 50;
+  this.attractor = 'lorentz';
+  this.respawn = 'false';
 }
 
 var point = function() {
@@ -14,30 +16,73 @@ var point = function() {
   this.z = Math.random()*40 - 20;
 }
 
+var randomize = { randomize:function(){ 
+}};
+
 // load gui
 var data = new data();
 var gui = new dat.GUI();
 
-var ctrlC = gui.add(data,'c',-20,50);
-var ctrlT = gui.add(data,'t',-20,20);
-var ctrlB = gui.add(data,'b',-10,10);
+var ctrlA = gui.add(data,'a',-20,50);
+var ctrlB = gui.add(data,'b',-20,20);
+var ctrlC = gui.add(data,'c',-10,10);
 var ctrlPoints = gui.add(data,'points',1,500,1);
+var ctrlAttractor = gui.add(data,'attractor',['lorentz','rossler','rabinovich-fabrikant']);
+var ctrlRespawn = gui.add(data,'respawn',['false','true']);
+var ctrlRand = gui.add(randomize,'randomize');
+var controls = [ctrlA,ctrlB,ctrlC,ctrlPoints,ctrlRespawn];
 
-var controls = [ctrlC,ctrlT,ctrlB,ctrlPoints];
+function updateDisplay() {
+    for (let i in gui.__controllers) {
+        gui.__controllers[i].updateDisplay();
+    }
+    for (let f in gui.__folders) {
+        updateDisplay(gui.__folders[f]);
+    }
+}
+
+var changeHandler = function() {
+  a = data.a;
+  b = data.b;
+  c = data.c;
+  points = data.points;
+
+  while(scene.children.length > 0){ 
+    scene.remove(scene.children[0]); 
+  }
+
+  updateDisplay();
+  genPoints();
+  initGeometry();
+}
+
+ctrlRand.onChange(function() {
+  data.c = (Math.random()*30)+6;
+  data.t = (Math.random()*2)-6;
+  data.b = (Math.random()*6)-1;
+  changeHandler();
+})
+
+ctrlAttractor.onChange(function() {
+  if(data.attractor=='lorentz') {
+    data.a = 28;
+    data.b = -6;
+    data.c = 2.67;
+  } else if(data.attractor=='rossler') {
+    data.a = 0.2;
+    data.b = 0.2;
+    data.c = 5.7;
+  } else if(data.attractor=='rabinovich-fabrikant') {
+    data.a = 0.87;
+    data.b = 1.1;
+    data.c = 0;
+  }
+  changeHandler();
+})
 
 for (var i = 0; i < controls.length; i++) {
   controls[i].onChange(function() {
-    c = data.c;
-    t = data.t;
-    b = data.b;
-    points = data.points;
-
-    while(scene.children.length > 0){ 
-      scene.remove(scene.children[0]); 
-    }
-
-    genPoints();
-    initGeometry();
+    changeHandler();
   });
 }
 
@@ -50,11 +95,11 @@ function onWindowResize() {
 }
 
 //define vars
-var dt = .015,
-c = data.c,t = data.t,b=data.b,
+var dt = .01,
+a = data.a, b= data.b,c=data.c,
 scale = 14,
 x0,y0,z0,x1, y1, z1,
-pointsArray, points=data.points, i=0,
+pointsArray, points=data.points,
 step=2,length=20,
 anim;
 
@@ -113,21 +158,43 @@ var update = function(k) {
   z0 = pointsArray[k].z;
 
   // remove divergent points
-  if( isNaN(x0) || isNaN(y0) || isNaN(z0) ) {
-    // pls re-impliment this to work for lineArray
-    pointsArray.splice(k,1);
-    scene.remove(lineArray[k]);
-    lineArray.splice(k,1);
-    points--;
+  if( isNaN(x0) || isNaN(y0) || isNaN(z0) || Math.abs(x0)>1000 || Math.abs(y0)>1000 || Math.abs(z0)>1000 ) {
+    console.log(data.respawn);
+    if(data.respawn=='true') {
+      pointsArray[k] = new point();
+    } else {
+      pointsArray.splice(k,1);
+      scene.remove(lineArray[k]);
+      lineArray.splice(k,1);
+      points--;
+    }
     return;
   }  
+
 
   // make a few new ones
   for(let j=0;j<step;j++) {
 
-    x1 = x0 + dt*t*(x0-y0);
-    y1 = y0 + dt*((-x0*z0+t)+(c*x0)-y0);
-    z1 = z0 + dt*((x0*y0)-b*z0);
+    switch(data.attractor) {
+      case 'lorentz':
+      x1 = x0 + dt*b*(x0-y0);
+      y1 = y0 + dt*((-x0*z0+b)+(a*x0)-y0);
+      z1 = z0 + dt*((x0*y0)-c*z0);
+      break;
+      case 'rossler':
+      x1 = x0 + dt*(-y0-z0);
+      y1 = y0 + dt*(x0+a*y0);
+      z1 = z0 + dt*(b+z0*(x0-c));
+      break;
+      case 'rabinovich-fabrikant':
+      x1 = x0 + dt*(y0*(z0-1+(x0*x0))+a*x0);
+      y1 = y0 + dt*(x0*(3*z0+1-(x0*x0))+a*x0);
+      z1 = z0 + dt*(-2*z0*(b+x0*y0));
+      default:
+      x1 = x0 + dt*b*(x0-y0);
+      y1 = y0 + dt*((-x0*z0+b)+(a*x0)-y0);
+      z1 = z0 + dt*((x0*y0)-c*z0);
+    }
 
     x0 = x1;
     y0 = y1;
@@ -140,11 +207,6 @@ var update = function(k) {
   lineArray[k].geometry.verticesNeedUpdate=true;
   var diff = Math.abs(x0-pointsArray[k].x)+Math.abs(y0-pointsArray[k].y)+Math.abs(z0-pointsArray[k].z);
   lineArray[k].material.color.setHSL(diff/20,.5,.5);
-    i++
-  // delete the trailing tip  
-  while(scene.children.length > 10*data.points){ 
-      scene.remove(scene.children[0]); 
-  }
 
   // import vals from the kth point
   pointsArray[k].x = x0;
@@ -157,7 +219,11 @@ var animate = function() {
   for(let k=0;k<points;k++) {
     update(k);
   }
-  if(i>10000) i=length;
+  // delete the trailing tip  
+  while(scene.children.length > length*data.points){ 
+      scene.remove(scene.children[0]); 
+  }
+  // then render
   render();
 }
 
